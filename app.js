9edaa301c21867327a31242e42a8f5abe0633a22
JayQkew <rayjebb@gmail.com>
2025-01-10 11:27:00 +0200

    parsing csv empty spaces

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 const fileInput = document.getElementById('xlsx-file-input');
 
 fileInput.addEventListener('change', (e) => {
-    const file = e.target.files[0];
+    const file = e.target.files[0]; // csv file
 
+    // process the data to something more readable
+    //      remove repeating applications (check names, emails, numbers)
+    //      display a new formatted table
     if(file){
-        console.log(`Name: ${file.name}`);
-        console.log(`Type: ${file.type}`)
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            const csvString = e.target.result;
+            const parsedData = parseCSV(csvString);
+
+            console.log(parsedData);
+        }
+        reader.readAsText(file);
     }
-})
+});
+
+function parseCSV(csvString){
+    const lines = csvString.split('\n');
+    const filteredLines = lines.filter(line => line.trim() !== '');
+    return filteredLines.map(line => line.split(','));
+}
