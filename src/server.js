b4b71b0456633f00fcb314a844368c81633dbfee
JayQkew <rayjebb@gmail.com>
2025-01-11 18:05:54 +0200

    updating to suit render

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const cors = require('cors');
 
 const app = express();
+const PORT = process.env.PORT || 8080;
 
+app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname, 'index.html')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
@@ -37,6 +40,6 @@ app.post('/api/tenants', (req, res) => {
     });
 });
 
-app.listen(8080, ()=>{
-    console.log('Server is listening on port 8080')
+app.listen(PORT, ()=>{
+    console.log(`Server is listening on port 8080 ${PORT}`)
 })
