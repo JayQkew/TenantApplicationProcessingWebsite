d92fe787bd89dcdc358d6301509a4ef89f4daf4c
JayQkew <rayjebb@gmail.com>
2025-01-12 20:15:57 +0200

    small problem fixed

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const { createClient } = require('@supabase/supabase-js');
-const cors = require(cors);
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
